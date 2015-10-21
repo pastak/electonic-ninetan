00f8af9f145213203066bd5fe702ad5b93e5adcb
pastak <pasta0915@gmail.com>
2015-10-21 13:58:46 +0900

    prepare for developing

---
+++ init.js
@@ -0,0 +1,14 @@
+var path = require('path')
+var argv = require('yargs')
+  .default('test', false)
+  .default('environment', 'production')
+  .argv
+require('crash-reporter').start()
+if (argv.environment == 'production') {
+  require('electron-compile').initForProduction(path.join(__dirname, 'compile-cache'))
+}
+else {
+  console.log('In development mode')
+  require('electron-compile').init()
+}
+require('./app/app')
