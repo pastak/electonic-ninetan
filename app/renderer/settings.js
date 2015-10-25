5d70dd1b44c591a1f45d4f822d74fce8aaa97495
pastak <pasta0915@gmail.com>
2015-10-25 21:51:04 +0900

    tweak

--- app/renderer/settings.js
+++ app/renderer/settings.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import SettingsApp from './components/SettingsApp'
 
-;(() => {
+(() => {
   document.addEventListener('DOMContentLoaded', () => {
     ReactDOM.render(<SettingsApp />, document.querySelector('#settingsApp'))
   })
